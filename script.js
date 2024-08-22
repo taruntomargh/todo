aeebe8f0c3dc68210de1cfe12f126d1e0bb91c4b
Tarun Tomar <101380624+taruntomargh@users.noreply.github.com>
2024-08-22 19:44:38 +0530

    Add edit todo function

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function addTodo() {
 function showTodos() {
   let todoList;
 
-  if(localStorage.length == 0){
+  if (localStorage.length == 0) {
     todoList = document.createElement("h3");
     todoList.textContent = "No todo here.";
     todos.appendChild(todoList);
@@ -30,20 +30,41 @@ function showTodos() {
       todoList = document.createElement("li");
       todoList.id = `todo${i}`;
       todoList.classList.add("todoList");
-      todoList.innerHTML = `<span>${localStorage.getItem(i)}</span><div class="edit-delete"><button class="" onclick="">Edit</button><button class="delete" onclick="singleDelete(${i})">Delete</button></div>`;
+      todoList.innerHTML = `<span id="todoContent${i}" class="todoContent">${localStorage.getItem(
+        i
+      )}</span><div class="edit-delete"><button class="edit-button" id="edit-btn-${i}" onclick="editTodo(${i})">Edit</button><button class="delete" onclick="singleDelete(${i})">Delete</button></div>`;
       todos.appendChild(todoList);
     }
   }
 }
 
-function singleDelete(key){
+function editTodo(key) {
+  const todoContent = document.getElementById(`todoContent${key}`);
+  const editState = todoContent.contentEditable = true;
+  todoContent.classList.add("editable");
+
+  const editBtn = document.getElementById(`edit-btn-${key}`);
+  editBtn.textContent = "Done";
+
+  editBtn.addEventListener("click", function () {
+  if (editState) {
+      localStorage.setItem(key, todoContent.textContent);
+      todoContent.contentEditable = false;
+      todoContent.classList.remove("editable");
+      editBtn.textContent = "Edit";
+      window.location.reload();
+    }
+  });
+}
+
+function singleDelete(key) {
   localStorage.removeItem(key);
   window.location.reload();
 }
 
 // Function to remove all todos
 function removeAllTodos() {
-  if(confirm("Are you sure? ")){
+  if (confirm("Are you sure? ")) {
     localStorage.clear();
     window.location.reload();
   }
@@ -59,4 +80,3 @@ removeAll.onclick = function () {
 
 // Function Calls
 showTodos();
-
