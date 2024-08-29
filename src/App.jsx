54760196bbf6c5531e0ba73434112146b48e8b64
Tarun Tomar <101380624+taruntomargh@users.noreply.github.com>
2024-08-29 20:44:14 +0530

    Add todo app and deleted App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleChange(event) {
+    setTask(event.target.value);
+    console.log(task);
+  }
+
+  async function handleAddTask() {
+    try {
+      const response = await axios.post("http://localhost:8000/add", {
+        data: {
+          task: task,
+        },
+      });
+      setTask("");
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function fetchAllTasks() {
+    try {
+      const response = await axios.get("http://localhost:8000/");
+      setTodos(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAllTasks();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl font-bold">TodoSync</h1>
+      <div className="flex gap-4 justify-center mt-36 w-[700px]">
+        <input
+          className="p-2 text-black bg-slate-200 rounded w-3/4 outline-none"
+          type="text"
+          placeholder="Add task"
+          onChange={handleChange}
+        />
+        <button
+          className="p-2 text-white rounded bg-blue-500"
+          onClick={handleAddTask}
+        >
+          Add task
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="flex flex-col gap-2 items-center mt-16 w-[700px]">
+        {todos.map((todo) => (
+          <div className="flex w-full p-2 m-0 bg-slate-200 rounded" key={todo.id}>
+            <li className="p-2 text-black w-3/4">{todo.task}</li>
+            <div className="flex gap-2">
+            <button className="p-2 text-white rounded bg-blue-500">Edit</button>
+            <button className="p-2 text-white rounded bg-red-500">
+              Delete
+            </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
